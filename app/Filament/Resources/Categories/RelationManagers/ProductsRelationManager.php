953f1e76d91b741d7ac84ed0e2402f6ec7e72b65
cybermanbg-cpu <cybermanbg@gmail.com>
2026-06-05 10:10:55 +0300

    category

--- app/Filament/Resources/Categories/RelationManagers/ProductsRelationManager.php
+++ app/Filament/Resources/Categories/RelationManagers/ProductsRelationManager.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Filament\Resources\CategoryResource\RelationManagers;
+namespace App\Filament\Resources\Categories\RelationManagers;
 
 use Filament\Actions\AttachAction;
 use Filament\Actions\BulkActionGroup;
